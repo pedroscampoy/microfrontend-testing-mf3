157b9f0c40b8e612f94a0df7d5b354b3f06424a7
DIR\pedro.j.sola.campoy <pedro.j.sola.campoy@accenture.com>
2022-04-20 20:06:14 +0200

    add style

--- ng-mfe3/apps/home/src/app/remote-entry/entry.component.ts
+++ ng-mfe3/apps/home/src/app/remote-entry/entry.component.ts
@@ -2,15 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ng-mfe3-home-entry',
-  template: `<div class="remote-entry">
-    <h2>home's Remote Entry Component</h2>
+  template: `<div class="expand">
+    <h2>home's Remote Entry Component from a different repository</h2>
   </div>`,
   styles: [
     `
-      .remote-entry {
-        background-color: #143055;
-        color: white;
-        padding: 5px;
+      .expand {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 100%;
+        background-color: azure;
       }
     `,
   ],
